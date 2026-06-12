7352a5c327a6a941a0873c82b66238ce649d0d2a
MrAhmed31 <240564@students.au.edu.pk>
2026-06-12 18:23:17 +0500

    Payment module integrated

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get("/orders", async (req, res) => {
     res.json(orders);
 });
 
+app.get("/payment", (req, res) => {
+    res.send("Payment Module Active");
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
