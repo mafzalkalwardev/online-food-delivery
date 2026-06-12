a12919b5ad0e5a46d29e321a55927af92dbc15df
mafzalkalwardev <243472@students.au.edu.pk>
2026-06-12 19:46:01 +0500

    Fixed test endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const Order = require("./models/Order");
 
 const app = express();
